aeb2a0186350fa9ad8579519b4f29898a4a31cdb
reesemulsid <96434391+reesemulsid@users.noreply.github.com>
2022-09-06 09:05:34 +0800

    Add files via upload

---
+++ q1ex1/Q1Ex1.java
@@ -0,0 +1,20 @@
+package q1ex1;
+
+public class Q1Ex1 {
+    public static void main(String[] args) {
+        String book1 = "If We Were Villains";
+        String language1 = "English";
+        String author1 = "M.L. Rio";
+        
+        String book2 = "The Invisible Life of Addie LaRue";
+        String language2 = "English";
+        String author2 = "V.E. Schwab";
+        
+        String book3 = "Every Day";
+        String language3 = "English";
+        String author3 = "David Levithan";
+        
+        System.out.print("#1 Favorite book" + /n + book1);
+    }
+    
+}
