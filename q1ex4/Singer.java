37e5c8b7e9776163f2a301d3d1e3c485406c72fe
reesemulsid <96434391+reesemulsid@users.noreply.github.com>
2022-10-06 11:25:47 +0800

    Update Singer.java

--- q1ex4/Singer.java
+++ q1ex4/Singer.java
@@ -1,3 +1,9 @@
+/*
+    Get the classes from Exercise 03 and clean them up, implementing the conventions mentioned (access modifiers, getter/setter methods).
+    Add a static field totalPerformances to the Singer class which counts all the performances of the Singers. Overload the 
+    performForAudience method such that Singers can also perform with another Singer and the profit will be split between the two of them.
+*/
+
 package q1ex4;
 
 public class Singer {
@@ -10,6 +16,13 @@ public class Singer {
         noOfPerformances += 1;
         earnings += audience * 100;
     }
+
+    public void performForAudience(String name1, String name2, int audience) {
+        noOfPerformances += 1;
+        earnings += audience * 100;
+        double earnings1 = earnings/2;
+        double earnings2 = earnings/2;
+    }
     
     public void changeFavSong(Song newFavSong) {
         favoriteSong = newFavSong;
@@ -20,6 +33,11 @@ public class Singer {
         noOfPerformances = num;
         earnings = e;
         favoriteSong = t;
+    }
+    
+    public static int totalPerformances() {
+        noOfPerformances += noOfPerformances;
+        return noOfPerformances;
     }
     
     public String getName(){
