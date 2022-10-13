2aa83b970a9592c8e8165ea6654f1d4139b17dd1
reesemulsid <96434391+reesemulsid@users.noreply.github.com>
2022-10-13 11:27:32 +0800

    Add files via upload

---
+++ q1ex5/Store.java
@@ -0,0 +1,61 @@
+package q1ex5;
+import java.util.*;
+
+public class Store {
+  private String name;
+  private double earnings;
+  private ArrayList<Item> itemList;
+  private static ArrayList<Store> storeList = new ArrayList();
+
+  public Store(String name){
+    this.name = name;
+    earnings = 0;
+    ArrayList<Store> itemList = new ArrayList();
+    storeList.add(this);
+  }
+
+  public String getName(){
+    return name;
+  }
+  public double getEarnings(){
+    return earnings;
+  }
+  public void sellItem(int index){
+      if (index <= itemList.size()) {
+          
+      }
+      
+      else {
+          System.out.println("There are only " + itemList.size() + " items in the store.");
+      }
+    // check if index is within the size of the itemList (if not, print statement that there are only x items in the store)
+    // get Item at index from itemList and add its cost to earnings
+    // print statement indicating the sale
+  }
+  public void sellItem(String name){
+    // check if Item with given name is in the itemList (you will need to loop over itemList) (if not, print statement that the store doesn't sell it)
+    // get Item from itemList and add its cost to earnings
+    // print statement indicating the sale
+  }
+  public void sellItem(Item i){
+    // check if Item i exists in the store (there is a method that can help with this) (if not, print statement that the store doesn't sell it)
+    // get Item i from itemList and add its cost to earnings
+    // print statement indicating the sale
+  }
+  public void addItem(Item i){
+    // add Item i to store's itemList
+  }
+  public void filterType(String type){
+    // loop over itemList and print all items with the specified type
+  }
+  public void filterCheap(double maxCost){
+    // loop over itemList and print all items with a cost lower than or equal to the specified value
+  }
+  public void filterExpensive(double minCost){
+    // loop over itemList and print all items with a cost higher than or equal to the specified value
+  }
+  public static void printStats(){
+    // loop over storeList and print the name and the earnings'Store.java'
+
+  }
+}
