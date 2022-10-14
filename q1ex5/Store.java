d350a57686ca0ab95dd92b458b63c82be40639c1
reesemulsid <96434391+reesemulsid@users.noreply.github.com>
2022-10-14 10:26:18 +0800

    Update Store.java

--- q1ex5/Store.java
+++ q1ex5/Store.java
@@ -1,4 +1,3 @@
-package q1ex5;
 import java.util.*;
 
 public class Store {
@@ -7,59 +6,81 @@ public class Store {
   private ArrayList<Item> itemList;
   private static ArrayList<Store> storeList = new ArrayList();
 
-  public Store(String name){
+  public Store(String name) {
     this.name = name;
     earnings = 0;
     ArrayList<Store> itemList = new ArrayList();
     storeList.add(this);
   }
 
-  public String getName(){
+  public String getName() {
     return name;
   }
-  public double getEarnings(){
+
+  public double getEarnings() {
     return earnings;
   }
-  public void sellItem(int index){
-      if (index <= itemList.size()) {
-          System.out.println("")
-      }
 
-      else {
-          System.out.println("There are only " + itemList.size() + " items in the store.");
-      }
-    // get Item at index from itemList and add its cost to earnings
-    // print statement indicating the sale
+  public void sellItem(int index) {
+    if (index < itemList.size()) {
+      Item product = itemList.get(index);
+      double itemCost = product.getCost();
+      double sale = earnings + itemCost;
+      System.out.println("Store's current sales are " + sale);
+    } else {
+      System.out.println("There are only " + itemList.size() + " items in the store.");
+    }
   }
-  public void sellItem(String name){
-    // check if Item with given name is in the itemList (you will need to loop over itemList) (if not, print statement that the store doesn't sell it)
+
+  public void sellItem(String name) {
+    for (Item i : itemList) {
+      if (i.contains(name)) {
+        Item product = itemList.get(index);
+        double itemCost = product.getCost();
+        double sale = earnings + itemCost;
+        System.out.println("Store's current sales are " + sale);
+      } else {
+        System.out.println("There are only " + itemList.size() + " items in the store.");
+      }
+    }
+    // check if Item with given name is in the itemList (you will need to loop over
+    // itemList) (if not, print statement that the store doesn't sell it)
     // get Item from itemList and add its cost to earnings
     // print statement indicating the sale
   }
-  public void sellItem(Item i){
-    // check if Item i exists in the store (there is a method that can help with this) (if not, print statement that the store doesn't sell it)
+
+  public void sellItem(Item i) {
+    // check if Item i exists in the store (there is a method that can help with
+    // this) (if not, print statement that the store doesn't sell it)
     // get Item i from itemList and add its cost to earnings
     // print statement indicating the sale
   }
-  public void addItem(Item i){
+
+  public void addItem(Item i) {
     itemList.add(i);
   }
-  public void filterType(String type){
+
+  public void filterType(String type) {
     // loop over itemList and print all items with the specified type
   }
-  public void filterCheap(double maxCost){
-    maxCost = Item getCost();
-    for (int i=0; i<itemList.size(); i++) {
-        if(itemList[i] > maxCost) {
-            maxCost = itemList[i];
-        }
+
+  public void filterCheap(double maxCost) {
+    maxCost = itemList.get(0);
+    for (int i = 0; i < itemList.size(); i++) {
+      if (itemList.get(i) > maxCost) {
+        maxCost = itemList[i];
+      }
     }
-    // loop over itemList and print all items with a cost lower than or equal to the specified value
+    // loop over itemList and print all items with a cost lower than or equal to the
+    // specified value
   }
-  public void filterExpensive(double minCost){
-    // loop over itemList and print all items with a cost higher than or equal to the specified value
+
+  public void filterExpensive(double minCost) {
+    // loop over itemList and print all items with a cost higher than or equal to
+    // the specified value
   }
-  public static void printStats(){
+
+  public static void printStats() {
     // loop over storeList and print the name and the earnings'Store.java'
 
   }
