f120d726988d9d878cff69e4b09468f821227ce9
reesemulsid <96434391+reesemulsid@users.noreply.github.com>
2022-10-14 13:27:43 +0800

    Update StoreSim.java

--- q1ex5/StoreSim.java
+++ q1ex5/StoreSim.java
@@ -1,13 +1,4 @@
-/*
-With what you’ve learned so far, you should be able to do the following:
-Create an array or ArrayList containing different data
-Loop over the different indices of the arrays and ArrayLists to check and use the content
-Given the attached Item.java, we can create a store, which is essentially just an array of Items. Complete
-the Store.java so that the application is able to work as specified in StoreSim.java;
-
-Submit the package in a compressed zip folder. The package should contain 3 java files, Store.java,
-Item.java, and the java file containing the main method.
-*/
+package q1ex5;
 
 public class StoreSim {
   public static void main(String[] args) {
@@ -57,7 +48,6 @@ public class StoreSim {
     mall.sellItem(i7);
     mall.sellItem(i3);
 
-    // Check final earnings
     Store.printStats();
   }
 }
