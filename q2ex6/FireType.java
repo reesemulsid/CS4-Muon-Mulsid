ccd2786f9b1e1ccbb4c1158926d84f7bb5d6e984
reesemulsid <96434391+reesemulsid@users.noreply.github.com>
2022-11-07 08:57:51 +0800

    Add files via upload

---
+++ q2ex6/FireType.java
@@ -0,0 +1,11 @@
+package q2ex6;
+/*
+a Monster that has a type "fire", strong against "grass", weak against "water", 
+and whose atk is 1.3 times the base value and def is 0.7 times the base value.
+The special of FireTypes increase their atk by 2 points at the cost of reducing their HP by 10% of their maxHP.
+the constructors of the subtypes will only require a name, HP value, and base value.
+You will also need to override the special method so that it functions according to what was specified.
+*/
+public class FireType extends Monster{
+    
+}
---
+++ q2ex6/GrassType.java
@@ -0,0 +1,11 @@
+package q2ex6;
+/*
+a Monster that has a type "grass", strong against "water", weak against "fire",
+and whose atk and def is equal to the base value. The rest() of GrassTypes heal by half the maxHP.
+The special of GrassTypes raise their health by 20% of the maxHP.
+the constructors of the subtypes will only require a name, HP value, and base value.
+You will also need to override the special method so that it functions according to what was specified.
+*/
+public class GrassType extends Monster{
+    
+}
